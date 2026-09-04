2e0cd712cad2c9270ed232b7dcc4619fd5eef6f9
onepiecebot <rodrigocortez729@gmail.com>
2026-09-04 02:58:52 -0300

    Corrijo server.js para que inicie el bot de Twitch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+// 1. Servidor HTTP (para que Render no se queje)
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Bot is alive!');
@@ -9,3 +10,7 @@ const PORT = process.env.PORT || 10000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor HTTP funcionando en el puerto ${PORT}`);
 });
+
+// 2. Iniciar el bot de Twitch (IMPORTANTE)
+console.log('Intentando iniciar el bot de Twitch...');
+require('./bot.js');
