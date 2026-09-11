dfabf33db24c0585e58266162b430913ee27602c
onepiecebot <rodrigocortez729@gmail.com>
2026-09-11 20:36:01 -0300

    Token con permisos de susurro

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     botName: 'op_d_bot',
-    oauth: 'oauth:lld5oie99qtdtlaqmz4xixuyd6b0cr',
+    oauth: 'oauth:cwwv7k7h7sw7keanipv4lrgl1l923d',
     channelName: 'Fan_D_LaRana'
 };
