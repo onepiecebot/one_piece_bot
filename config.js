6f31032c3ab9f0d7c38da60a7eb118bc8dddcb7c
onepiecebot <rodrigocortez729@gmail.com>
2026-09-12 01:01:09 -0300

    Fix: token corregido de nuevo

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     botName: 'op_d_bot',
-    oauth: 'oauth:qfub7iou125s11ylg5brm7e7drh8q7',
+    oauth: 'oauth:ap3b75qcl4dlkhqkogg6ru2uv31g32',
     channelName: 'Fan_D_LaRana'
 };
